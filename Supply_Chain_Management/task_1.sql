1f49e1f4031fe1766c05e6da42a725db7c754049
holydexboi <holydexboi@yahoo.com>
2024-10-05 08:58:48 +0100

    task

--- Supply_Chain_Management/task_1.sql
+++ Supply_Chain_Management/task_1.sql
@@ -1,7 +1,44 @@
 
 --- TOTAL QUANTITY OF PRODUCT SHIPPED BY EACH PRODUCT CATEGORY
 
-SELECT p."productCategory", SUM(s.quantity) as total_quantity_shipped
+SELECT pr."productCategory", SUM(sh.quantity) AS qty
+FROM shipment sh
+INNER JOIN product pr
+ON sh."productId" = pr."productId"
+GROUP BY pr."productCategory"
+ORDER BY qty DESC
+
+--- WAREHOUSE WITH THE MOST EFFICIENT SHIPPING PROCESS BASE ON AVERAGE SHIPPING TIME 
+SELECT wr."warehouseName", AVG(sh."shippingDuration") AS avgDuration
+FROM shipment sh
+INNER JOIN warehouse wr
+ON sh."warehouseId" = wr."warehouseId"
+GROUP BY wr."warehouseName"
+HAVING AVG(sh."shippingDuration") < (SELECT AVG("shippingDuration") FROM shipment)
+ORDER BY AVG(sh."shippingDuration") DESC
+
+--- TOTAL VALUE OF SHIPMENT FOR EACH SUPPLIER
+SELECT sup."supplierName", SUM(s."shipmentValue") AS shipValue
 FROM shipment s
-JOIN product p ON s."productId" = p."productId"
-GROUP BY p."productCategory";
+INNER JOIN supplier sup
+ON s."supplierId" = sup."supplierId"
+GROUP BY sup."supplierName"
+ORDER BY shipValue DESC
+
+--- TOP 5 PRODUCTS WITH THE HEIGHEST QUANTITY OF SHIPMENT
+SELECT pr."productName", SUM(sh.quantity) AS totalQuatity
+FROM shipment sh
+INNER JOIN product pr
+ON sh."productId" = pr."productId"
+GROUP BY pr."productName"
+ORDER BY totalQuatity DESC
+LIMIT 5
+
+--- REPORT OF TOTAL VALUE OF SHIPMENT FOR EACH PRODUCT CATEGORY
+SELECT pr."productCategory", SUM(sh."shipmentValue") AS totalValue
+FROM shipment sh
+INNER JOIN product pr
+ON sh."productId" = pr."productId"
+GROUP BY pr."productCategory"
+
+
